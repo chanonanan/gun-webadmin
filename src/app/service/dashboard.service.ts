708e42c50eebbdfd55ef279cbcca11f9384be58f
chanonanan <chanonanan@gmail.com>
2019-04-19 14:33:39 +0700

    demo api

--- src/app/service/dashboard.service.ts
+++ src/app/service/dashboard.service.ts
@@ -5,14 +5,14 @@ import { api as apiConst } from '../constant';
 @Injectable({
   providedIn: 'root'
 })
-export class WebStatService {
+export class DashboardService {
 
   constructor(private http: HttpClient) { }
   test() {
     return this.http.get(apiConst.TEST_WEBSTAT_URL);
   }
 
-  getWeb(start,end) {
+  get(start,end) {
     return this.http.get(apiConst.HOST + apiConst.WEB_URL + start + '/'+end);
   }
 }
